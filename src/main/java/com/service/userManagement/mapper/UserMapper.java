f200d55c1988f26b6f9a46e0de982046e63e29f0
Mostafa123165 <mostafatrek87@gmail.com>
2024-12-05 15:39:06 +0200

    Add governorate andCity in register

--- src/main/java/com/service/userManagement/mapper/UserMapper.java
+++ src/main/java/com/service/userManagement/mapper/UserMapper.java
@@ -2,11 +2,13 @@ package com.service.userManagement.mapper;
 
 import com.service.auth.model.UserRegisterDto;
 import com.service.base.mapper.BaseMapper;
+import com.service.common.mapper.CityMapper;
+import com.service.common.mapper.GovernorateMapper;
 import com.service.userManagement.dto.UserDto;
 import com.service.userManagement.model.User;
 import org.mapstruct.Mapper;
 
-@Mapper(uses = {UserTypeMapper.class})
+@Mapper(uses = {UserTypeMapper.class, CityMapper.class, GovernorateMapper.class})
 public interface UserMapper extends BaseMapper<User, UserDto> {
 
     User unMapRegister(UserRegisterDto t);
