357e8a6c5a9c110cfb61d39e1b5ca5e6040ac5bb
Mostafa123165 <mostafatrek87@gmail.com>
2025-02-14 14:42:48 +0200

    Disable the building materials supplier user. Get all the user types by status code.

--- src/main/java/com/service/userManagement/repository/UserTypeReps.java
+++ src/main/java/com/service/userManagement/repository/UserTypeReps.java
@@ -2,8 +2,19 @@ package com.service.userManagement.repository;
 
 import com.service.base.repository.BaseLkpRepository;
 import com.service.userManagement.model.UserType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserTypeReps extends BaseLkpRepository<UserType,Integer> {
+
+    @Override
+    @Query(value = """
+    SELECT userType
+    FROM  UserType userType
+    WHERE userType.statusCode = 5
+    """)
+    List<UserType> findAll();
 }
