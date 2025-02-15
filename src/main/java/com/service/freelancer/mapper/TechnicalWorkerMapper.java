ddb0e5454a5f5bc1259e3a9dc8868e246bfa7403
Mohamed Magdy <du.khdma@gmail.com>
2025-02-15 20:59:43 +0200

    engineering office setup

--- src/main/java/com/service/freelancer/mapper/TechnicalWorkerMapper.java
+++ src/main/java/com/service/freelancer/mapper/TechnicalWorkerMapper.java
@@ -1,12 +1,17 @@
 package com.service.freelancer.mapper;
 
 import com.service.base.mapper.BaseMapper;
+import com.service.common.mapper.CityMapper;
+import com.service.common.mapper.GovernorateMapper;
 import com.service.freelancer.dto.TechnicalWorkerDto;
 import com.service.freelancer.model.TechnicalWorker;
 import com.service.userManagement.mapper.UserMapper;
+import com.service.userManagement.mapper.UserTypeMapper;
 import org.mapstruct.Mapper;
 import org.springframework.context.i18n.LocaleContextHolder;
 
-@Mapper(componentModel = "spring",imports = {LocaleContextHolder.class , UserMapper.class,TechnicalWorkerServMapper.class,TechnicalWorkerTypeMapper.class})
+@Mapper(imports = {LocaleContextHolder.class} ,
+         uses = { UserMapper.class,TechnicalWorkerServMapper.class,TechnicalWorkerTypeMapper.class
+                 ,UserTypeMapper.class, CityMapper.class, GovernorateMapper.class})
 public interface TechnicalWorkerMapper extends BaseMapper<TechnicalWorker, TechnicalWorkerDto> {
 }
