1419bb458586e21d2488aff3e49d107831d2ffd2
Mohamed Magdy <du.khdma@gmail.com>
2025-06-26 00:43:46 +0300

    top

--- src/main/java/com/service/freelancer/service/EngineerService.java
+++ src/main/java/com/service/freelancer/service/EngineerService.java
@@ -1,11 +1,18 @@
 package com.service.freelancer.service;
 
+import com.service.base.model.SearchRequest;
 import com.service.base.service.BaseServiceImpl;
 import com.service.freelancer.model.Engineer;
 import com.service.freelancer.repository.EngineerReps;
 import com.service.userManagement.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +25,31 @@ public class EngineerService extends BaseServiceImpl<Engineer, Long> {
     public Engineer findByUserId(Long userId) {
         if (userId == null) userId = userService.getCurrentUser().getId();
         return engineerReps.findByUserId(userId);
+    }
+
+    public Page<Engineer> filterTop(Optional<SearchRequest> request) {
+        SearchRequest req = request.orElse(new SearchRequest());
+
+        Number governorateId = null;
+        Number cityId = null;
+        Number engineerTypeId = null;
+        Number engineerServId = null;
+        Integer yearsOfExperience = null;
+        Integer rate = null;
+
+        if (req.getSearchCriteria() != null && !req.getSearchCriteria().isEmpty()) {
+            governorateId = (Integer) req.getSearchCriteria().getOrDefault("governorateId", null);
+            cityId = (Integer) req.getSearchCriteria().getOrDefault("cityId", null);
+            engineerTypeId = (Integer) req.getSearchCriteria().getOrDefault("engineerTypeId", null);
+            yearsOfExperience = (Integer) req.getSearchCriteria().getOrDefault("yearsOfExperience", null);
+            engineerServId = (Integer) req.getSearchCriteria().getOrDefault("engineerServId", null);
+            rate = (Integer) req.getSearchCriteria().getOrDefault("rate", null);
+        }
+
+        Sort sort = Sort.by(req.getSortableType() == 2 ? Sort.Direction.DESC : Sort.Direction.ASC, req.getSortableColumn());
+
+        Pageable pageable = PageRequest.of(req.getPageNumber(), req.getPageSize(), sort);
+
+        return engineerReps.filterTop(governorateId, cityId, engineerTypeId, yearsOfExperience, engineerServId, rate, pageable);
     }
 }
--- src/main/java/com/service/freelancer/service/EngineeringOfficeService.java
+++ src/main/java/com/service/freelancer/service/EngineeringOfficeService.java
@@ -1,12 +1,19 @@
 package com.service.freelancer.service;
 
+import com.service.base.model.SearchRequest;
 import com.service.base.service.BaseServiceImpl;
 import com.service.freelancer.mapper.EngineeringOfficeMapper;
 import com.service.freelancer.model.EngineeringOffice;
 import com.service.freelancer.repository.EngineeringOfficeReps;
 import com.service.userManagement.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +21,43 @@ public class EngineeringOfficeService extends BaseServiceImpl<EngineeringOffice,
 
     private final UserService userService;
     private final EngineeringOfficeReps engineeringOfficeReps;
-    private final EngineeringOfficeMapper engineeringOfficeMapper;
 
     public EngineeringOffice findByUserId(Long userId) {
         if (userId == null) userId = userService.getCurrentUser().getId();
         EngineeringOffice engineeringOffice = engineeringOfficeReps.findByUserId(userId);
         return engineeringOffice;
+    }
+
+
+    public Page<EngineeringOffice> filterTop(Optional<SearchRequest> request) {
+        SearchRequest req = request.orElse(new SearchRequest());
+
+        Number governorateId = null;
+        Number cityId = null;
+        Number engineeringOfficeFieldId = null;
+        Number engineeringOfficeDepartmentId = null;
+        Integer rate = null;
+
+        if (req.getSearchCriteria() != null && !req.getSearchCriteria().isEmpty()) {
+            governorateId = (Integer) req.getSearchCriteria().getOrDefault("governorateId", null);
+            cityId = (Integer) req.getSearchCriteria().getOrDefault("cityId", null);
+            engineeringOfficeFieldId = (Integer) req.getSearchCriteria().getOrDefault("engineeringOfficeFieldId", null);
+            engineeringOfficeDepartmentId = (Integer) req.getSearchCriteria().getOrDefault("engineeringOfficeDepartmentId", null);
+            rate = (Integer) req.getSearchCriteria().getOrDefault("rate", null);
+        }
+
+        Sort sort = Sort.by(req.getSortableType() == 2 ? Sort.Direction.DESC : Sort.Direction.ASC,
+                req.getSortableColumn());
+
+        Pageable pageable = PageRequest.of(req.getPageNumber(), req.getPageSize(), sort);
+
+        return engineeringOfficeReps.filterTop(
+                governorateId,
+                cityId,
+                engineeringOfficeFieldId,
+                engineeringOfficeDepartmentId,
+                rate,
+                pageable
+        );
     }
 }
