e736d395f871af13830dee4f749d69a4980967b2
Mohamed Magdy <120112354+MOhamedMaGdy20820@users.noreply.github.com>
2025-07-05 21:37:52 +0300

    add request asks

--- src/main/java/com/service/freelancer/controller/RequestAskEngineerController.java
+++ src/main/java/com/service/freelancer/controller/RequestAskEngineerController.java
@@ -1,6 +1,7 @@
 package com.service.freelancer.controller;
 
 import com.service.base.controller.BaseControllerImpl;
+import com.service.base.model.SuccessResponse;
 import com.service.base.model.SuccessResponseList;
 import com.service.freelancer.dto.RequestAskEngineerDto;
 import com.service.freelancer.mapper.RequestAskEngineerMapper;
@@ -9,10 +10,7 @@ import com.service.freelancer.service.RequestAskEngineerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +27,15 @@ public class RequestAskEngineerController extends BaseControllerImpl<RequestAskE
     public ResponseEntity<?> getMyAsks(@RequestParam Long askId) {
         List<RequestAskEngineerDto> dtos = requestAskEngineerMapper.map(requestAskEngineerService.getByAskId(askId));
         return ResponseEntity.ok(new SuccessResponseList<RequestAskEngineerDto>(dtos));
+    }
+
+    @Override
+    public ResponseEntity<?> update(@RequestBody RequestAskEngineerDto dto) {
+        RequestAskEngineer entity1 = requestAskEngineerService.findById(dto.getId());
+        RequestAskEngineer entity2 = requestAskEngineerMapper.UpdateMap(dto, entity1);
+        RequestAskEngineer result = requestAskEngineerService.update(entity2);
+        RequestAskEngineerDto dtos = requestAskEngineerMapper.map(result);
+        return ResponseEntity.ok(new SuccessResponse<>(dtos));
     }
 }
 
--- src/main/java/com/service/freelancer/mapper/RequestAskWorkerMapper.java
+++ src/main/java/com/service/freelancer/mapper/RequestAskWorkerMapper.java
@@ -4,11 +4,17 @@ import com.service.base.mapper.BaseMapper;
 import com.service.freelancer.dto.RequestAskWorkerDto;
 import com.service.freelancer.model.RequestAskWorker;
 import com.service.userManagement.mapper.UserMapper;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 @Mapper(imports = {LocaleContextHolder.class},
         uses = {UserMapper.class, AskWorkerMapper.class})
 public interface RequestAskWorkerMapper extends BaseMapper<RequestAskWorker, RequestAskWorkerDto> {
 
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    RequestAskWorker UpdateMap(RequestAskWorkerDto requestAskWorkerDto, @MappingTarget RequestAskWorker askWorker);
 }
