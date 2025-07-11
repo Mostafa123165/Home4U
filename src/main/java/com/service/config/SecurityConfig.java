de5712578b9f78e178314f543f3df8f4480aa7f2
Mohamed Magdy <120112354+MOhamedMaGdy20820@users.noreply.github.com>
2025-07-12 00:50:38 +0300

    add request asks

--- src/main/java/com/service/config/SecurityConfig.java
+++ src/main/java/com/service/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
             "/api/v1/engineer-services/service/**",
             "/api/v1/engineering-office-field/**",
             "/api/v1/engineering-office-department/field/**",
-            "api/v1/products/recommended-for-you/**",
+            "/api/v1/products/recommended-for-you/**",
             "/api/v1/business-config/**",
             "api/v1/products/top-best-seller/**",
             "api/v1/products/highest-rated/**",
